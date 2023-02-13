0ef5d6df2a76f5e6c9cb2d2556d60b0552296e64
Fortuna <110213201+rafablaster@users.noreply.github.com>
2023-02-13 01:58:38 -0300

    Add files via upload

---
+++ tabelha hash.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int valor;
+    int chave;
+    struct node *prox;
+}oi;
+
+oi no[10];
+
+void inicializa() {
+    int i;
+    for (i = 0; i < 10; i++) {
+        no[i].chave = -1;
+        no[i].valor = -1;
+        no[i].prox = NULL;
+    }
+}
+
+unsigned int hash(int chave){
+    return chave % 10;
+}
+
+void insere(int chave, int valor) {
+    int b = hash(chave);
+    if (no[b].chave == -1) {
+        no[b].chave = chave;
+        no[b].valor = valor;
+    } else {
+        oi *novo = (oi*) malloc(sizeof(struct node));
+        novo->chave = chave;
+        novo->valor = valor;
+        novo->prox = no[b].prox;
+        no[b].prox = novo;
+    }
+}
+
+void exibe() {
+    int i;
+    oi *temp;
+    for (i = 0; i < 10; i++) {
+        printf("Indice %d: ", i);
+        if (no[i].chave != -1) {
+            printf("(%d, %d)", no[i].chave, no[i].valor);
+            temp = no[i].prox;
+            while (temp != NULL) {
+                printf(" -> (%d, %d)", temp->chave, temp->valor);
+                temp = temp->prox;
+            }
+        }
+        printf("\n");
+    }
+}
+
+int busca(int chave, int valor){
+    int b = hash(chave);
+    int index = b;
+
+    while (no[index].chave != -1) {
+        if (no[index].valor == valor) {
+            return index;
+        }
+        if (no[index].prox != NULL) {
+            oi *temp = no[index].prox;
+            while (temp != NULL) {
+                if (temp->valor == valor) {
+                    return index;
+                }
+                temp = temp->prox;
+            }
+        }
+        index = (index + 1) % 10;
+    }
+
+    printf("Nao encontrado!\n");
+    return -1;
+}
+
+void imprime() {
+    int i;
+    for (i = 0; i < 10; i++) {
+        printf("Indice %d: Chave %d Valor %d\n", i, no[i].chave, no[i].valor);
+    }
+}
+
+int main(){
+    inicializa();
+    insere(1, 10);
+    insere(2, 20);
+    insere(3, 30);
+    insere(4, 40);
+    imprime();
+    int resultado = busca(3, 30);
+    if (resultado != -1) {
+        printf("Elemento encontrado na posicao %d\n", resultado);
+    }
+    return 0;
+}
